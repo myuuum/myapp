eadb4aee35cb70fd55f085403c710418749296c8
myuuum <yuu20001229@gmail.com>
2022-03-16 09:37:53 +0900

    first commit

---
+++ database/migrations/2022_03_15_122349_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps(date);
+            $table->string('body_comment', 200);
+            $table->string('body_time', 50);
+            $table->increments('taget_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
